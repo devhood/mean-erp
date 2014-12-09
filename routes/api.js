cc00af7b882d6d2c41562930bdfff49761caaf74
facascante <facascante@smart.com.ph>
2014-12-09 18:23:05 +0800

    merge

--- routes/api.js
+++ routes/api.js
@@ -72,6 +72,23 @@ router.use(function(req, res, next) {
         res.status(400).json(err);
       });
 })
+.get('/print/:object/:id', function(req, res) {
+  var id = mongoq.mongodb.BSONPure.ObjectID.createFromHexString(req.params.id);
+  req.query.filter = JSON.parse(req.query.filter || '{}');
+  req.query.columns = JSON.parse(req.query.columns || '{}');
+  req.query.sorting = JSON.parse(req.query.sorting || '{}');
+  req.query.filter._id = id;
+  db.collection(req.params.object)
+  .find(req.query.filter,req.query.columns || {})
+  .sort(req.query.sorting || {}).skip(req.query.page || 0)
+  .limit(req.query.rows || 0).toArray()
+  .done(function(data){
+    res.status(200).json(data[0]);
+  })
+  .fail( function( err ) {
+    res.status(400).json(err);
+  });
+})
 .put('/:object/:id/upload',multipartMiddleware, function(req, res) {
   var id = mongoq.mongodb.BSONPure.ObjectID.createFromHexString(req.params.id);
   if(req.files){
