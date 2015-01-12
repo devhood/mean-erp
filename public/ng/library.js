5ba61a6d61e737ad20569f4fe88ee51f84dcdda9
acebesmonte <acebesmonte@gmail.com>
2015-01-13 00:03:13 +0800

    update permission

--- public/ng/library.js
+++ public/ng/library.js
@@ -85,7 +85,7 @@ angular.module('erp')
     },
     Permission : {
       isAllowed : function(client,path){
-        var allowed = true;
+        var allowed = false;
         for(var i in client.permissions){
           if(path.indexOf(client.permissions[i].route) != -1){
             allowed = true;
