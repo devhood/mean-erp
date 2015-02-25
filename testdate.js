4a832eff699c9a90d525a2c2a4b53b88a4de7dae
acebesmonte <acebesmonte@gmail.com>
2015-02-25 20:06:52 +0800

    fix customer form

--- testdate.js
+++
@@ -1,6 +0,0 @@
-
-var fDate = new Date("2015-02-04T05:33:34.949Z");
-// d. = ;
-// var n = d.toLocaleDateString("en-US");
-// var n = d.toDateString("en-US");
-console.log((fDate.getMonth() + 1) + "/" + fDate.getDate() + "/" + fDate.getFullYear()+"  "+  fDate.getHours()+":"+fDate.getMinutes()+":"+fDate.getSeconds());
