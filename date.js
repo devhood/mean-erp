69d07738b9a0fec3fa274348a72786f800609cfd
acebesmonte <acebesmonte@gmail.com>
2015-03-25 16:58:07 +0800

    fix date in inventory transaction history

---
+++ date.js
@@ -0,0 +1,3 @@
+var fDate = new Date();
+fDate =(fDate.getMonth() + 1) + "/" + fDate.getDate() + "/" + fDate.getFullYear()+" "+  fDate.getHours()+":"+fDate.getMinutes()+":"+fDate.getSeconds();
+console.log(fDate);
--- taskdate.js
+++ taskdate.js
@@ -65,13 +65,19 @@ extract sales:
 
 
 //pldt evaline catigbak 09213835770 8364140
+
 var fDate = new Date("2015-02-04T05:33:34.949Z");
 // d. = ;
 // var n = d.toLocaleDateString("en-US");
 // var n = d.toDateString("en-US");
+
 console.log((fDate.getMonth() + 1) + "/" + fDate.getDate() + "/" + fDate.getFullYear()+"  "+  fDate.getHours()+":"+fDate.getMinutes()+":"+fDate.getSeconds());
 
 
+///\
+new Date().toISOString().
+  replace(/T/, ' ').      // replace T with a space
+  replace(/\..+/, '')
 
 
 
