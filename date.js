72dbfe17101f0171ca59d0d97eea05a4835ca0e5
acebesmonte <acebesmonte@gmail.com>
2015-03-26 13:36:34 +0800

    fix payment form

--- date.js
+++ date.js
@@ -1,3 +1,370 @@
-var fDate = new Date();
-fDate =(fDate.getMonth() + 1) + "/" + fDate.getDate() + "/" + fDate.getFullYear()+" "+  fDate.getHours()+":"+fDate.getMinutes()+":"+fDate.getSeconds();
-console.log(fDate);
+{
+  _id:"54dafde78622445a4909a639",
+  customer:{
+    _id:"54dabefae1ff103526e6e9d1",
+    company_name:"Nakamura Beauty International Corp. ",
+    branch:"Taguig City",
+    type:"Professional",
+    trade_name:"",
+    tin:"0000000000000000",
+    commission_sharing:"00",
+    sec_no:"none",
+    phone:"",
+    email:"none.gmail.com",
+    discount:"None",
+    shipping_mode:"ideliver101",
+    sales_executive:"Maricel Orticio",
+    status:"active",
+    price_type:"Professional",
+    shipping_address:{
+      landmark:"",
+      barangay:"F1 Hotel 32nd St. Bonifacio Global",
+      country:"Philippines",
+      province:"Metro Manila",
+      city:"Taguig City",
+      zipcode:""
+    },
+    billing_address:{
+      landmark:"",
+      barangay:"F1 Hotel 32nd St. Bonifacio Global",
+      country:"Philippines",
+      province:"Metro Manila",
+      city:"",
+      zipcode:""
+    },
+    contacts:[
+      {
+        name:"n/a",
+        position:"",
+        email:""
+      }
+    ],
+    credit_limit:"0",
+    transaction_limit:"1",
+    payment_term:"CASH",
+    website:""
+  },
+  discount:0,
+  total_vat:2600,
+  total_amount_due:2600,
+  zero_rate_sales:0,
+  withholding_tax:0,
+  inventory_location:"54cada6fe1ff103526e6b8e7",
+  special_instruction:"From Consignment of Maricel Orticio "  Print DR & SI Only! Reference DR# 12941 CR#2729"",
+  transaction_type:"Professional",
+  ordered_items:[
+    {
+      _id:"54cb19f8e1ff103526e6c9f4",
+      bl_code:"FBG01",
+      brand:"Fabulash",
+      name:"Premium Glue",
+      size:"10ml",
+      color:"Black",
+      uom:"Piece",
+      part_number:"FBPNO5_PREMIUMGLUE",
+      payment_term:"",
+      description:"N/A",
+      supplier_code:"G01",
+      Status:"Active",
+      movement:"C",
+      supplier:"iBeauty",
+      international_cost:5,
+      retail_price:0,
+      professional_price:400,
+      product_photo:"products/FBPNO5_PREMIUMGLUE.jpg",
+      audit_history:[
+        {
+          user:{
+            _id:"547ba578e1ff103526e637e9",
+            username:"chito",
+            fullname:"Chito Cascante",
+            position:"Developer",
+            email:"chito.cascante@gmail.com",
+            phone:639000000000,
+            status:"Active",
+            permission:{
+              _id:"54bcf72de1ff103526e6a12d",
+              group:"Customers",
+              name:"Edit Customer",
+              route:"/customer/edit"
+            }
+          },
+          update_date:"2015-01-30T07:56:11.407Z",
+          module:"products"
+        }
+      ],
+      quantity:1,
+      override:"NORMAL",
+      price:400,
+      total:400
+    },
+    {
+      _id:"54cb19f8e1ff103526e6c9f5",
+      bl_code:"FBG02",
+      brand:"Fabulash",
+      name:"Ultra Bonding Glue",
+      size:"10ml",
+      color:"Black",
+      uom:"Piece",
+      part_number:"FBPNO6_ULTRABONDINGGLUE",
+      payment_term:"",
+      description:"Finish this first stock first! Next order will be from a different supplier! Inform DDT whenever stock is about to finish.",
+      supplier_code:"G02",
+      Status:"Active",
+      movement:"C",
+      supplier:"iBeauty",
+      international_cost:6,
+      retail_price:0,
+      professional_price:600,
+      product_photo:"products/FBPNO6_ULTRABONDINGGLUE.jpg",
+      audit_history:[
+        {
+          user:{
+            _id:"547ba578e1ff103526e637e9",
+            username:"chito",
+            fullname:"Chito Cascante",
+            position:"Developer",
+            email:"chito.cascante@gmail.com",
+            phone:639000000000,
+            status:"Active",
+            permission:{
+              _id:"54bcf72de1ff103526e6a12d",
+              group:"Customers",
+              name:"Edit Customer",
+              route:"/customer/edit"
+            }
+          },
+          update_date:"2015-01-30T07:56:11.716Z",
+          module:"products"
+        }
+      ],
+      quantity:1,
+      override:"NORMAL",
+      price:600,
+      total:600
+    },
+    {
+      _id:"54cb19f8e1ff103526e6c9f2",
+      bl_code:"FBG03",
+      brand:"Fabulash",
+      name:"Sensitive Glue",
+      size:"10ml",
+      color:"Black",
+      uom:"Piece",
+      part_number:"FBPNO3_SENSITIVEGLUE1",
+      payment_term:"",
+      description:"Finish this first stock first! Next order will be from a different supplier! Inform DDT whenever stock is about to finish.",
+      supplier_code:"G03",
+      Status:"Active",
+      movement:"C",
+      supplier:"iBeauty",
+      international_cost:6.5,
+      retail_price:0,
+      professional_price:700,
+      product_photo:"products/FBPNO3_SENSITIVEGLUE1.jpg",
+      audit_history:[
+        {
+          user:{
+            _id:"547ba578e1ff103526e637e9",
+            username:"chito",
+            fullname:"Chito Cascante",
+            position:"Developer",
+            email:"chito.cascante@gmail.com",
+            phone:639000000000,
+            status:"Active",
+            permission:{
+              _id:"54bcf72de1ff103526e6a12d",
+              group:"Customers",
+              name:"Edit Customer",
+              route:"/customer/edit"
+            }
+          },
+          update_date:"2015-02-05T09:20:18.638Z",
+          module:"products"
+        }
+      ],
+      quantity:1,
+      override:"NORMAL",
+      price:700,
+      total:700
+    },
+    {
+      _id:"54cb19f8e1ff103526e6c9f7",
+      bl_code:"FBEB",
+      brand:"Fabulash",
+      name:"Primer",
+      size:"15ml",
+      color:"N/A",
+      uom:"Piece",
+      part_number:"FBPNO8_PRIMER1",
+      payment_term:"",
+      description:"N/A",
+      supplier_code:"EB",
+      Status:"Active",
+      movement:"C",
+      supplier:"iBeauty",
+      international_cost:1.8,
+      retail_price:0,
+      professional_price:280,
+      product_photo:"products/FBPNO8_PRIMER1.jpg",
+      audit_history:[
+        {
+          user:{
+            _id:"547ba578e1ff103526e637e9",
+            username:"chito",
+            fullname:"Chito Cascante",
+            position:"Developer",
+            email:"chito.cascante@gmail.com",
+            phone:639000000000,
+            status:"Active",
+            permission:{
+              _id:"54bcf72de1ff103526e6a12d",
+              group:"Customers",
+              name:"Edit Customer",
+              route:"/customer/edit"
+            }
+          },
+          update_date:"2015-01-30T07:56:11.420Z",
+          module:"products"
+        }
+      ],
+      quantity:1,
+      override:"NORMAL",
+      price:280,
+      total:280
+    },
+    {
+      _id:"54cb19f8e1ff103526e6c9f9",
+      bl_code:"FBGR01",
+      brand:"Fabulash",
+      name:"Gel Remover",
+      size:"15ml",
+      color:"N/A",
+      uom:"Piece",
+      part_number:"FBPNO10_GEL REMOVER1",
+      payment_term:"",
+      description:"Finish this first stock first! Next order will be from a different supplier! Inform DDT whenever stock is about to finish.",
+      supplier_code:"GR01",
+      Status:"Active",
+      movement:"C",
+      supplier:"iBeauty",
+      international_cost:3,
+      retail_price:0,
+      professional_price:320,
+      product_photo:"products/FBPNO10_GEL REMOVER1.jpg",
+      audit_history:[
+        {
+          user:{
+            _id:"547ba578e1ff103526e637e9",
+            username:"chito",
+            fullname:"Chito Cascante",
+            position:"Developer",
+            email:"chito.cascante@gmail.com",
+            phone:639000000000,
+            status:"Active",
+            permission:{
+              _id:"54bcf72de1ff103526e6a12d",
+              group:"Customers",
+              name:"Edit Customer",
+              route:"/customer/edit"
+            }
+          },
+          update_date:"2015-01-30T07:56:06.071Z",
+          module:"products"
+        }
+      ],
+      quantity:1,
+      override:"NORMAL",
+      price:320,
+      total:320
+    },
+    {
+      _id:"54cb19f8e1ff103526e6c977",
+      bl_code:"FBC15-10",
+      brand:"Fabulash",
+      name:"Mink Eyelash Extensions, 12 lines Tray, C-curl ",
+      size:"0.15*10mm",
+      color:"Black",
+      uom:"Piece",
+      part_number:"FBPNO1_MINKTRAY",
+      payment_term:"",
+      description:"N/A",
+      supplier_code:"N/A",
+      Status:"Active",
+      movement:"C",
+      supplier:"Qingdao Hairbeauty Arts & Crafts Co., Ltd.",
+      international_cost:2.3,
+      retail_price:0,
+      professional_price:300,
+      product_photo:"products/FBPNO1_MINKTRAY.jpg",
+      audit_history:[
+        {
+          user:{
+            _id:"547ba578e1ff103526e637e9",
+            username:"chito",
+            fullname:"Chito Cascante",
+            position:"Developer",
+            email:"chito.cascante@gmail.com",
+            phone:639000000000,
+            status:"Active",
+            permission:{
+              _id:"54bcf72de1ff103526e6a12d",
+              group:"Customers",
+              name:"Edit Customer",
+              route:"/customer/edit"
+            }
+          },
+          update_date:"2015-01-30T07:56:08.426Z",
+          module:"products"
+        }
+      ],
+      quantity:1,
+      override:"NORMAL",
+      price:300,
+      total:300
+    }
+  ],
+  subtotal:2600,
+  isNeedApproval:false,
+  totalQuantity:6,
+  reference_no:"SI#6882 DR#13019",
+  order_source:"Sales Executive",
+  ordered_by:"Maricel Orticio",
+  delivery_method:"Walk- In",
+  delivery_date:"2015-01-22T16:00:00.000Z",
+  status:{
+    status_code:"PAYMENT_CREATED",
+    status_name:"Accounting-Payment created"
+  },
+  sono:"SO121-2015",
+  audit_history:[
+    {
+      user:{
+        _id:"54d452784a8deb9836a202c7",
+        fullname:"Jendelle Anne Amante",
+        username:"jamante",
+        position:"Accounting Staff",
+        email:"jamante@beautylane.ph",
+        phone:6327710771,
+        status:"Active"
+      },
+      update_date:"2015-03-16T06:47:20.988Z",
+      module:"sales"
+    }
+  ],
+  pckno:"PACK132-2015",
+  drno:"DR123-2015",
+  sino:"SI127-2015",
+  payment_notes:"PRICE ADJUSTMENT",
+  payment_details:[
+    {
+      payment_type:"Cash",
+      check_number:"CR2729",
+      check_dep_date:"2015-01-20T16:00:00.000Z",
+      bank:"BDO",
+      amount:2910
+    }
+  ],
+  total_payment:2910,
+  pmno:"PYMT154-2015"
+}
